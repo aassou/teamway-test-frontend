46a6068c7b00d68aaf2dd6df552ec6d8752b65b7
aassou <aassou.abdelilah@gmail.com>
2022-04-30 16:11:17 +0200

    Fixing some issues

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: process.env.PERSONALITY_TEST_API_URI || `http://127.0.0.1:8000/`
+    baseURL: process.env.PERSONALITY_TEST_API_URI || `https://teamway-api.herokuapp.com/api`
 });
 
 export default axiosInstance;
